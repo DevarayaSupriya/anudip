5a9587fe75d1c466a60a22f2a3fde49f9508fa06
Devaraya Supriya <devaraya24365@gmail.com>
2024-07-19 18:34:00 +0530

    Add files via upload

---
+++ ArthematicOperaters.java
@@ -0,0 +1,18 @@
+package apjfsa;
+
+public class ArthematicOperaters {
+	
+	  public static void main(String[] args)
+{
+	   int a = 12, b = 5;
+	   
+System.out.println("a + b = " + (a + b));
+System.out.println("a - b = " + (a - b));
+System.out.println("a * b = " + (a * b));
+System.out.println("a / b = " + (a / b));
+System.out.println("a % b = " + (a % b));
+	  }
+	}
+
+
+
